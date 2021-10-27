1779e8e096811e65a57477af5606e2172131423a
JCMiguel <juancristianmiguel@gmail.com>
2021-10-26 23:33:05 -0300

    Primera maqueta de pantalla de login

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="Crypto Actas"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
